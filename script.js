86007603296649c05550e07ca6d4baca3d48b7fc
obscuremax <maxxam@yandex.ru>
2020-03-15 12:13:50 +0300

    feat: add slide switch

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const menu = document.getElementById("menu")
+const left = document.getElementById("left")
+const right = document.getElementById("right")
+const slider = document.getElementById("slider")
+const slide1 = document.getElementById("slide1")
+
 
 menu.addEventListener("click", (event)=>{
 	menu.querySelectorAll("a").forEach(el => el.classList.remove("active"))
 	event.target.classList.add("active")
+})
+
+left.addEventListener("click", ()=>{
+	slide1.classList.toggle("toogle")
+	slider.classList.toggle("toogle_section")
+})
+right.addEventListener("click", ()=>{
+	slide1.classList.toggle("toogle")
+	slider.classList.toggle("toogle_section")
 })
