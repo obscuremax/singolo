c217195d19a48c2708978330d65c8f450dede25c
obscuremax <maxxam@yandex.ru>
2020-03-15 16:04:04 +0300

    feat: add Tab Switching

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ const phone_v = document.getElementById("phone_v")
 const phone_g = document.getElementById("phone_g")
 const black_v = document.getElementById("black_v")
 const black_g = document.getElementById("black_g")
+const portfolio_nav = document.getElementById("portfolio_nav")
+
+
 
 menu.addEventListener("click", (event)=>{
 	menu.querySelectorAll("a").forEach(el => el.classList.remove("active"))
 	event.target.classList.add("active")
 })
+
+portfolio_nav.addEventListener("click", (event)=>{
+	portfolio_nav.querySelectorAll("span").forEach(el=> el.classList.remove("active"))
+	event.target.classList.add("active")
+
+	const divImages = document.getElementById("images")
+	const images = document.querySelectorAll("#images img")
+	let newArr = []
+	images.forEach((el,index) => index === 0? "":newArr.push(el.outerHTML))
+	images.forEach((el,index) => index === 0? newArr.push(el.outerHTML):"")
+	divImages.innerHTML = newArr.join("")
+})
+
 
 left.addEventListener("click", ()=>{
 	slide2.classList.toggle("toogle")
